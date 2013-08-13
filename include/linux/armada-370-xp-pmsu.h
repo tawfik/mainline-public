7f2254980b8e730ee2e6ce1ed208f158698d62b0
Gregory CLEMENT <gregory.clement@free-electrons.com>
2013-08-13 15:49:26 +0200

    ARM: mvebu: Allow to power down L2 cache controller in idle mode
    This commit adds an exported function which allows to power down the
    L2 cache controller and the Coherency Fabric when the CPU goes into
    deep idle mode.
    
    This feature is part of the Power Management Service Unit of the
    Armada 370 and Armada XP SoCs.
    
    Signed-off-by: Gregory CLEMENT <gregory.clement@free-electrons.com>

---
+++ include/linux/armada-370-xp-pmsu.h
@@ -0,0 +1,16 @@
+/*
+ * Power Management Service Unit (PMSU) support for Armada 370/XP platforms.
+ *
+ * Copyright (C) 2013 Marvell
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2.  This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#ifndef __ARMADA_370_XP__PMSU_H
+#define __ARMADA_370_XP__PMSU_H
+
+void armada_370_xp_pmsu_enable_l2_powerdown_onidle(void);
+
+#endif	/* __ARMADA_370_XP__PMSU_H */
