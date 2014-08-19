573e68917c6dcbcb870dda26ebe5aa13bf637180
Gregory CLEMENT <gregory.clement@free-electrons.com>
2014-08-19 17:57:39 +0200

    clk: mvebu: Fix clk frequency value if SSCG is enabled
    When the SSCG (Spread Spectrum Clock Generator) is enabled, it shifts
    the frequency of the clock. The percentage is nor more than 1% but
    when the clock is used for a timer it leads to a clock drift.
    
    This patch allows to correct the affected clock when the SSCG is
    enabled. The check is done in an new optional function related to each
    SoC: is_sscg_enabled(). If this function is not present then no
    correction is done on the clocks.
    
    Signed-off-by: Gregory CLEMENT <gregory.clement@free-electrons.com>

--- drivers/clk/mvebu/common.h
+++ drivers/clk/mvebu/common.h
@@ -28,6 +28,7 @@ struct coreclk_soc_desc {
 	u32 (*get_tclk_freq)(void __iomem *sar);
 	u32 (*get_cpu_freq)(void __iomem *sar);
 	void (*get_clk_ratio)(void __iomem *sar, int id, int *mult, int *div);
+	bool (*is_sscg_enabled)(void __iomem *sar);
 	const struct coreclk_ratio *ratios;
 	int num_ratios;
 };
